e5164ed48ebcaaa70dd3f2341fcb9e6855299d24
Nestor <nestor.chumania@epn.edu.ec>
2023-02-13 12:05:39 -0500

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,11 @@
+public class Login extends javax.swing.JDialog {
+private javax.swing.JPanel contentPane;
+private javax.swing.JButton buttonOK;
+private javax.swing.JButton buttonCancel;
+
+public Login(){
+setContentPane(contentPane);
+setModal(true);
+getRootPane().setDefaultButton(buttonOK);
+}
+}
